3aaf976e7dd589922f46184101b1d13b394affae
sleal-unity <steven.leal@unity3d.com>
2021-03-31 13:45:10 -0500

    cleaned up addressable loading in SynthDetScenario

--- SynthDet/Assets/Scripts/Runtime/SynthDetScenario.cs
+++ SynthDet/Assets/Scripts/Runtime/SynthDetScenario.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using SynthDet.Randomizers;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -8,7 +10,6 @@ using UnityEngine.Perception.GroundTruth;
 using UnityEngine.Perception.Randomization.Parameters;
 using UnityEngine.Perception.Randomization.Scenarios;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceLocations;
 
 namespace SynthDet.Scenarios
 {
@@ -19,11 +20,10 @@ namespace SynthDet.Scenarios
     [AddComponentMenu("SynthDet/SynthDet Scenario")]
     public class SynthDetScenario : FixedLengthScenario
     {
-        int m_NumPrefabsToLoad;
-        List<GameObject> m_Prefabs = new List<GameObject>();
-        AssetLoadingStatus m_LoadingStatus = AssetLoadingStatus.LoadingCatalog;
-        AsyncOperationHandle<IResourceLocator>[] m_CatalogHandles;
-
+        static HashSet<Color> s_ColorsAlreadyUsed = new HashSet<Color>();
+        static ColorRgbParameter s_SemanticColorParameter = new ColorRgbParameter();
+        
+        AssetLoadingStatus m_LoadingStatus = AssetLoadingStatus.InProgress;
         List<string> m_LabelStringsForAutoLabelConfig = new List<string>();
 
         /// <inheritdoc/>
@@ -31,54 +31,11 @@ namespace SynthDet.Scenarios
         {
             get
             {
-                switch (m_LoadingStatus)
-                {
-                    case AssetLoadingStatus.Complete:
-                        return true;
-                    case AssetLoadingStatus.LoadingCatalog:
-                    {
-                        foreach (var handle in m_CatalogHandles)
-                        {
-                            if (!handle.IsDone)
-                                return false;
-                        }
-                        
-                        foreach (var handle in m_CatalogHandles)
-                        {
-                            if (handle.Status != AsyncOperationStatus.Succeeded)
-                            {
-                                Debug.LogError("Catalog download failed");
-                                m_LoadingStatus = AssetLoadingStatus.Failed;
-                                return false;
-                            }
-                        }
-                        
-                        LoadAndLabelPrefabs();
-                        m_LoadingStatus = AssetLoadingStatus.LoadingPrefabs;
-                        return false;
-                    }
-                    case AssetLoadingStatus.LoadingPrefabs:
-                    {
-                        lock (m_Prefabs)
-                        {
-                            if (m_Prefabs.Count < m_NumPrefabsToLoad)
-                                return false;
-                            SetupLabelConfigs();
-                            var randomizer = GetRandomizer<ForegroundObjectPlacementRandomizer>();
-                            m_Prefabs.Sort((prefab1, prefab2) => prefab1.name.CompareTo(prefab2.name));
-                            randomizer.prefabs = m_Prefabs.ToArray();
-                            m_LoadingStatus = AssetLoadingStatus.Complete;
-                            return true;
-                        }
-                    }
-                    case AssetLoadingStatus.Failed:
-                    {
-                        Quit();
-                        return false;
-                    }
-                    default:
-                        return false;
-                }
+                if (m_LoadingStatus == AssetLoadingStatus.Complete)
+                    return true;
+                if (m_LoadingStatus == AssetLoadingStatus.Failed)
+                    Quit();
+                return false;
             }
         }
 
@@ -86,75 +43,79 @@ namespace SynthDet.Scenarios
         {
             base.OnAwake();
 
-            Caching.ClearCache();
+            // Map bundle urls from app-param to addressables resource locations.
+            // This mapping enables dynamically assigned urls to be used for remote bundle locations.
+            Addressables.InternalIdTransformFunc = location => m_BundleToUrlMap.ContainsKey(location.PrimaryKey)
+                ? m_BundleToUrlMap[location.PrimaryKey]
+                : location.InternalId;
+            
+            LoadCatalogs();
+        }
 
-            string InternalIdTransformFunc(IResourceLocation location)
+        async void LoadCatalogs()
+        {
+            // Begin loading the remote content catalogs included in the app-param
+            var catalogHandles = new List<AsyncOperationHandle<IResourceLocator>>();
+            foreach (var url in m_CatalogUrls)
             {
-                var internalId = m_BundleToUrlMap.ContainsKey(location.PrimaryKey)
-                    ? m_BundleToUrlMap[location.PrimaryKey]
-                    : location.InternalId;
-                
-                Debug.Log($"InternalIdTransformFunc. Old: {location.InternalId} New: {internalId}");
-                return internalId;
+                var catalogHandle = Addressables.LoadContentCatalogAsync(url, false);
+                catalogHandles.Add(catalogHandle);
             }
 
-            Addressables.InternalIdTransformFunc = InternalIdTransformFunc;
+            await Task.WhenAll(catalogHandles.Select(handle => handle.Task));
 
-            // Addressables.InternalIdTransformFunc = TransformFunc;
-            
-            m_CatalogHandles = new AsyncOperationHandle<IResourceLocator>[m_CatalogUrls.Count];
-            for(var i = 0 ; i < m_CatalogUrls.Count; i++)
-                m_CatalogHandles[i] = Addressables.LoadContentCatalogAsync(m_CatalogUrls[i], false);
+            var allSucceeded = catalogHandles.All(handle => handle.Status == AsyncOperationStatus.Succeeded);
+            if (allSucceeded)
+            {
+                var catalogs = catalogHandles.Select(completedHandle => completedHandle.Result);
+                LoadAndLabelPrefabs(catalogs);
+            }
+            else
+            {
+                Debug.LogError("Catalogs failed to load");
+                m_LoadingStatus = AssetLoadingStatus.Failed;
+            }
         }
         
-        string TransformFunc(IResourceLocation location)
+        void LoadAndLabelPrefabs(IEnumerable<IResourceLocator> catalogs)
         {
-            Debug.Log(location.InternalId);
-            if (location.InternalId.Contains("catalog") && location.InternalId.Contains(".json"))
-                return location.InternalId;
-            if (location.InternalId.Contains("__placeholder__") && location.InternalId.Contains("_assets_all_")) //"doorgroup6_assets_all_ef13245136c2b5bec4ea18ac92071dca.bundle"))
-                return location.InternalId.Replace("__placeholder__/foreground_group_assets_all_0c7829f95130438cde35283652f1fd3f.bundle", "https://storage.googleapis.com/addressables-synthdet/e2e/newe2e/foreground_group_assets_all_0c7829f95130438cde35283652f1fd3f.bundle");
-            if (location.InternalId.Contains("__placeholder__") && location.InternalId.Contains("_unitybuiltinshaders_"))
-                //"doorgroup6_unitybuiltinshaders_fb3cc65dc055f6c5ef84d30de128788a.bundle"))
-                return location.InternalId.Replace("__placeholder__/foreground_group_unitybuiltinshaders_51bd82438b9ab720fa52c2dbd163e111.bundle","https://storage.googleapis.com/addressables-synthdet/e2e/newe2e/foreground_group_unitybuiltinshaders_51bd82438b9ab720fa52c2dbd163e111.bundle");
-            return location.InternalId;
-        }
-
-        void LoadAndLabelPrefabs()
-        {
-            foreach (var handle in m_CatalogHandles)
+            // Gather only the prefab keys from the remote catalogs
+            var prefabKeys = new List<string>();
+            foreach (var catalog in catalogs)
             {
-                foreach (var key in handle.Result.Keys)
+                foreach (var key in catalog.Keys)
                 {
-                    if (!key.ToString().Contains(".prefab"))
-                        continue;
-                    
-                    m_NumPrefabsToLoad++;
-                    Addressables.LoadAssetAsync<GameObject>(key).Completed += prefabHandle =>
-                    {
-                        if (prefabHandle.Status == AsyncOperationStatus.Failed)
-                        {
-                            Debug.LogError($"Failed to load prefab from key '{key}'");
-                            m_LoadingStatus = AssetLoadingStatus.Failed;
-                            return;
-                        }
-                        lock (m_Prefabs)
-                        {
-                            ConfigureLabeling(prefabHandle.Result);
-                            prefabHandle.Result.layer = LayerMask.NameToLayer("Foreground");
-                            m_Prefabs.Add(prefabHandle.Result);
-                        }
-                    };
+                    var keyPath = key.ToString();
+                    if (keyPath.Contains(".prefab"))
+                        prefabKeys.Add(keyPath);
                 }
             }
-        }
-
-        enum AssetLoadingStatus
-        {
-            Complete,
-            LoadingCatalog,
-            LoadingPrefabs,
-            Failed
+            
+            // Load all the prefabs from the remote catalogs, label them, and finally assign
+            // them to the ForegroundObjectPlacementRandomizer.
+            Addressables.LoadAssetsAsync<GameObject>(
+                prefabKeys, null, Addressables.MergeMode.Union, false).Completed += handle =>
+            {
+                if (handle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    SetupLabelConfigs();
+                
+                    var prefabsList = new List<GameObject>(handle.Result);
+                    prefabsList.Sort((prefab1, prefab2) => prefab1.name.CompareTo(prefab2.name));
+                    foreach (var prefab in prefabsList)
+                        ConfigureLabeling(prefab);
+                
+                    var randomizer = GetRandomizer<ForegroundObjectPlacementRandomizer>();
+                    randomizer.prefabs = prefabsList.ToArray();
+                
+                    m_LoadingStatus = AssetLoadingStatus.Complete;
+                }
+                else
+                {
+                    Debug.LogError("Prefabs failed to load");
+                    m_LoadingStatus = AssetLoadingStatus.Failed;
+                }
+            };
         }
 
         void ConfigureLabeling(GameObject gObj)
@@ -225,13 +186,11 @@ namespace SynthDet.Scenarios
                 labeler.Init(perceptionCamera);
             }
         }
-
-        static HashSet<Color> s_ColorsAlreadyUsed = new HashSet<Color>();
-        static ColorRgbParameter s_SemanticColorParameter = new ColorRgbParameter();
+        
         static Color GetUniqueSemanticSegmentationColor()
         {
             var sampledColor = s_SemanticColorParameter.Sample();
-            var maxTries = 1000;
+            const int maxTries = 1000;
             var count = 0;
 
             while (s_ColorsAlreadyUsed.Contains(sampledColor) && count <= maxTries)
@@ -243,6 +202,13 @@ namespace SynthDet.Scenarios
 
             s_ColorsAlreadyUsed.Add(sampledColor);
             return sampledColor;
+        }
+        
+        enum AssetLoadingStatus
+        {
+            InProgress,
+            Complete,
+            Failed
         }
     }
 }
