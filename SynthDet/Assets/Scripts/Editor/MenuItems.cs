5d6022a376b5c3347f9932274865c723bccee9e2
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-04-16 12:21:08 -0700

    Added an option to open the SynthDet scene to the SynthDet menu

--- SynthDet/Assets/Scripts/Editor/MenuItems.cs
+++ SynthDet/Assets/Scripts/Editor/MenuItems.cs
@@ -1,8 +1,11 @@
-﻿using SynthDet.Randomizers;
+﻿using System;
+using SynthDet.Randomizers;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers;
 using UnityEngine.Perception.Randomization.Scenarios;
+using UnityEngine.SceneManagement;
 using BackgroundObjectPlacementRandomizer = SynthDet.Randomizers.BackgroundObjectPlacementRandomizer;
 using ForegroundObjectPlacementRandomizer = SynthDet.Randomizers.ForegroundObjectPlacementRandomizer;
 
@@ -10,14 +13,28 @@ namespace SynthDet
 {
     public static class MenuItems
     {
-        [MenuItem("SynthDet/Create Default Scenario")]
+        [MenuItem("SynthDet/Open SynthDet Scene")]
+        static void OpenSynthDetScene()
+        {
+            try
+            {
+                EditorSceneManager.OpenScene("Assets/Scenes/SynthDet.unity");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Could not open the SynthDet Scene. Make sure the file Assets/Scenes/SynthDet.unity exists and is a valid Scene.");
+                Debug.LogException(e);
+            }
+        }
+        
+        [MenuItem("SynthDet/Recreate Default SynthDet Scenario")]
         static void CreateDefaultScenario()
         {
             var scenarioObj = new GameObject("Scenario");
 
             var scenario = scenarioObj.AddComponent<FixedLengthScenario>();
             scenario.constants.totalIterations = 1000;
-            
+
             scenario.AddRandomizer(new BackgroundObjectPlacementRandomizer());
             scenario.AddRandomizer(new ForegroundObjectPlacementRandomizer());
             scenario.AddRandomizer(new ForegroundOccluderPlacementRandomizer());
