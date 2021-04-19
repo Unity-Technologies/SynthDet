94653b8190a833969d33ed6913fc848a5f20153f
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-04-19 09:47:48 -0700

    removed scenario creation function from SynthDet menu

--- SynthDet/Assets/Scripts/Editor/MenuItems.cs
+++ SynthDet/Assets/Scripts/Editor/MenuItems.cs
@@ -25,32 +25,6 @@ namespace SynthDet
                 Debug.LogError("Could not open the SynthDet Scene. Make sure the file Assets/Scenes/SynthDet.unity exists and is a valid Scene.");
                 Debug.LogException(e);
             }
-        }
-        
-        [MenuItem("SynthDet/Recreate Default SynthDet Scenario")]
-        static void CreateDefaultScenario()
-        {
-            var scenarioObj = new GameObject("Scenario");
-
-            var scenario = scenarioObj.AddComponent<FixedLengthScenario>();
-            scenario.constants.totalIterations = 1000;
-
-            scenario.AddRandomizer(new BackgroundObjectPlacementRandomizer());
-            scenario.AddRandomizer(new ForegroundObjectPlacementRandomizer());
-            scenario.AddRandomizer(new ForegroundOccluderPlacementRandomizer());
-            scenario.AddRandomizer(new ForegroundOccluderScaleRandomizer());
-            scenario.AddRandomizer(new ForegroundScaleRandomizer());
-            scenario.AddRandomizer(new TextureRandomizer());
-            scenario.AddRandomizer(new HueOffsetRandomizer());
-            scenario.AddRandomizer(new RotationRandomizer());
-            scenario.AddRandomizer(new UnifiedRotationRandomizer());
-            scenario.AddRandomizer(new LightRandomizer());
-            scenario.AddRandomizer(new CameraRandomizer());
-            scenario.AddRandomizer(new ForegroundObjectMetricReporter());
-            scenario.AddRandomizer(new LightingInfoMetricReporter());
-            scenario.AddRandomizer(new CameraPostProcessingMetricReporter());
-
-            Selection.activeGameObject = scenarioObj;
-        }
+        }               
     }
 }
