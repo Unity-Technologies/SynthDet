a1c33b37ee2da33c56936a10d0155e63f5de6b2a
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-04-26 10:58:05 -0700

    added metrics, has newtonsoft json issue

---
+++ SynthDet_V2/Assets/Scripts/ForegroundObjectMetricReporterTag.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Perception.Randomization.Randomizers;
+
+namespace SynthDet.RandomizerTags
+{
+    [AddComponentMenu("SynthDet/RandomizerTags/My Foreground Object Metric Reporter Tag")]
+    public class ForegroundObjectMetricReporterTag : RandomizerTag { }
+}
+
---
+++ SynthDet_V2/Assets/Scripts/LightingInfoMetricReporterTag.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Perception.Randomization.Randomizers;
+
+namespace SynthDet.RandomizerTags
+{
+    [AddComponentMenu("SynthDet/RandomizerTags/My Lighting Info Metric Reporter Tag")]
+    public class LightingInfoMetricReporterTag : RandomizerTag { }
+}
+
