215dc6f0cfadaf62bc29510bbd1ea3adfb2b1957
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-05-10 10:28:42 -0700

    This commit has bounds normalization disabled, foreground scale randomizer ranges changed to match

--- SynthDet/Assets/Scripts/Randomizers/ForegroundScaleRandomizerForNormalizedMeshes.cs
+++ SynthDet/Assets/Scripts/Randomizers/ForegroundScaleRandomizerForNormalizedMeshes.cs
@@ -8,17 +8,17 @@ using UnityEngine.Perception.Randomization.Samplers;
 namespace SynthDet.Randomizers
 {
     [Serializable]
-    [AddRandomizerMenu("SynthDet/Foreground Scale Randomizer")]
-    public class ForegroundScaleRandomizer : Randomizer
+    [AddRandomizerMenu("SynthDet/Foreground Scale Randomizer for Normalized Object Bounds")]
+    public class ForegroundScaleRandomizerForNormalizedMeshes : Randomizer
     {
-        public FloatParameter scale = new FloatParameter { value = new UniformSampler(4f, 8f) };
+        public FloatParameter scale = new FloatParameter { value = new UniformSampler(0.15f, 0.60f) };
 
         protected override void OnIterationStart()
         {
             var tags = tagManager.Query<ForegroundScaleRandomizerTag>();
             foreach (var tag in tags)
             {
-                tag.transform.localScale = Vector3.one * scale.Sample();
+                tag.transform.localScale *= scale.Sample();
             }
         }
     }
